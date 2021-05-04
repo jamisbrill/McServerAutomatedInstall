d989b043a044107d54282e94b3f5ce159be1d93c
Sam Towers <jamisbrillx@gmail.com>
2021-05-04 11:07:38 +0100

    Update mcinstall.sh

--- mcinstall.sh
+++ mcinstall.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 echo "please run with sudo"
 sudo apt-get install default-jdk #install java
-useradd -m -d /home/mc -s /bin/bash -c "mc server" -U mc #create a seggregated user
+useradd -m -d /home/mc -s /bin/bash -c "mc server user" -U mc #create a seggregated user
 echo "please enter a pass for your new user"
 passwd mc # User sets password
 mkdir /home/mc/serverfiles #create directory to store server files
-cd /home/mc/serverfiles # navigate top dir
-curl --output spigot.jar https://cdn.getbukkit.org/spigot/spigot-1.16.5.jar # Download spigot
+cd /home/mc/serverfiles # navigate to dir
+curl --output spigot.jar https://cdn.getbukkit.org/spigot/spigot-1.16.5.jar # Download spigot // Update this path 
 echo "java -Xms1024M -Xmx3024M -jar spigot.jar -o false -nogui" > launch.sh   # Append To file
 chmod +x launch.sh    # Make file executeable
 sudo ufw allow 25565  #Open mc port
